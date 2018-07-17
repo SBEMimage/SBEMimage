058304778ef2cea2ee34091469d1254cea14e6c7
btitze <btitze@protonmail.ch>
2018-07-18 00:56:02 +0200

    Update: two new entries

--- src/config_template.py
+++ src/config_template.py
@@ -17,7 +17,7 @@
 # deleted from the default configuration files
 CFG_TEMPLATE_FILE = '..\\cfg\\default.ini'
 CFG_NUMBER_SECTIONS = 10
-CFG_NUMBER_KEYS = 176
+CFG_NUMBER_KEYS = 178
 
 SYSCFG_TEMPLATE_FILE = '..\\cfg\\system.cfg'
 SYSCFG_NUMBER_SECTIONS = 7
