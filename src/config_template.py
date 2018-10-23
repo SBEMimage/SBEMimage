429f5cb1ebfd8b2c53b95413b4e546499a629248
btitze <btitze@protonmail.ch>
2018-10-23 16:53:15 +0200

    Update ini and syscfg with SEM stage parameters

--- src/config_template.py
+++ src/config_template.py
@@ -17,11 +17,11 @@
 # deleted from the default configuration files
 CFG_TEMPLATE_FILE = '..\\cfg\\default.ini'
 CFG_NUMBER_SECTIONS = 10
-CFG_NUMBER_KEYS = 181
+CFG_NUMBER_KEYS = 192
 
 SYSCFG_TEMPLATE_FILE = '..\\cfg\\system.cfg'
 SYSCFG_NUMBER_SECTIONS = 7
-SYSCFG_NUMBER_KEYS = 23
+SYSCFG_NUMBER_KEYS = 24
 
 import os
 from configparser import ConfigParser
