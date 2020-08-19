555f16a3d866527f408982f1272794b0ac81a23b
btitze <btitze@protonmail.ch>
2020-08-19 18:33:05 +0200

    Update all header comments
    Use a simplified header comment that links to the repository, includes a copyright notice, and licence info.
    
    No date or version number to avoid a maintenance burden. Only end year of copyright notice will need to be updated once a year.

--- src/test_overview_manager.py
+++ src/test_overview_manager.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
+# ==============================================================================
+#   This source file is part of SBEMimage (github.com/SBEMimage)
+#   (c) 2018-2020 Friedrich Miescher Institute for Biomedical Research, Basel,
+#   and the SBEMimage developers.
+#   This software is licensed under the terms of the MIT License.
+#   See LICENSE.txt in the project root folder.
+# ==============================================================================
+
 """Tests for overview_manager.py"""
 
 import sys
