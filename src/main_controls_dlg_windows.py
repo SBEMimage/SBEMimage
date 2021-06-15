5cda6cfdbd32bd311412faf566fde761f4289709
Benjamin Titze <btitze@users.noreply.github.com>
2021-06-15 23:57:21 +0200

    Remove input restrictions in two dialogs

--- src/main_controls_dlg_windows.py
+++ src/main_controls_dlg_windows.py
@@ -1321,8 +1321,8 @@ class MagCalibrationDlg(QDialog):
         self.setWindowIcon(QIcon('..\\img\\icon_16px.ico'))
         self.setFixedSize(self.size())
         self.show()
-        self.spinBox_calibrationFactor.setValue(
-            self.sem.MAG_PX_SIZE_FACTOR)
+        self.lineEdit_calibrationFactor.setText(
+            str(self.sem.MAG_PX_SIZE_FACTOR))
         self.comboBox_frameWidth.addItems(['2048', '4096'])
         self.comboBox_frameWidth.setCurrentIndex(1)
         self.pushButton_calculate.clicked.connect(
@@ -1335,17 +1335,17 @@ class MagCalibrationDlg(QDialog):
         frame_width = int(str(self.comboBox_frameWidth.currentText()))
         pixel_size = self.doubleSpinBox_pixelSize.value()
         mag = self.spinBox_mag.value()
-        new_factor = mag * frame_width * pixel_size
+        new_factor = int(mag * frame_width * pixel_size)
         user_choice = QMessageBox.information(
             self, 'Calculated calibration factor',
             'Result:\nNew magnification calibration factor: %d '
             '\n\nDo you want to use this value?' % new_factor,
             QMessageBox.Ok | QMessageBox.Cancel)
         if user_choice == QMessageBox.Ok:
-            self.spinBox_calibrationFactor.setValue(new_factor)
+            self.lineEdit_calibrationFactor.setText(str(new_factor))
 
     def accept(self):
-        self.sem.MAG_PX_SIZE_FACTOR = self.spinBox_calibrationFactor.value()
+        self.sem.MAG_PX_SIZE_FACTOR = int(self.lineEdit_calibrationFactor.text())
         super().accept()
 
 # ------------------------------------------------------------------------------
