80c59f27abaaa5f3f6153625a3faef3d6c8ea611
Benjamin Titze <btitze@users.noreply.github.com>
2021-08-01 19:06:17 +0200

    Update test_load_config.py

--- src/test_load_config.py
+++ src/test_load_config.py
@@ -2,25 +2,25 @@
 
 # ==============================================================================
 #   This source file is part of SBEMimage (github.com/SBEMimage)
-#   (c) 2018-2020 Friedrich Miescher Institute for Biomedical Research, Basel,
+#   (c) 2018-2021 Friedrich Miescher Institute for Biomedical Research, Basel,
 #   and the SBEMimage developers.
 #   This software is licensed under the terms of the MIT License.
 #   See LICENSE.txt in the project root folder.
 # ==============================================================================
 
-"""Load the default user and system configuration."""
+"""Load the default session and system configuration."""
 
 import os
 from configparser import ConfigParser
 
 from config_template import process_cfg
 
-# Load default user and system configurations
+# Load default session and system configurations
 config = ConfigParser()
-with open(os.path.join('..', 'cfg', 'default.ini'), 'r') as file:
+with open(os.path.join('..', 'src', 'default_cfg', 'default.ini'), 'r') as file:
     config.read_file(file)
 sysconfig = ConfigParser()
-with open(os.path.join('..', 'cfg', 'system.cfg'), 'r') as file:
+with open(os.path.join('..', 'src', 'default_cfg', 'system.cfg'), 'r') as file:
     sysconfig.read_file(file)
 
 def test_process_config():
